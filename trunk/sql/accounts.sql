185228a5947e73c6d4e89b164353cba8b4e012c9
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2009-06-13 04:37:04 +0000

    Adding account maintenance.

--- trunk/sql/accounts.sql
+++ trunk/sql/accounts.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `private_accounts` (
+CREATE TABLE `accounts` (
   `id` bigint(20) NOT NULL auto_increment,
-  `year` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL default '',  
+  `datum` date NOT NULL default '2006-07-01',
   `saldo` decimal(10,2) NOT NULL default '0.00',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
