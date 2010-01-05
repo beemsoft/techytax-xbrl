b9d4f19a3d5307831f27ed33ed8320c352078c48
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2010-01-05 19:57:16 +0000

    Changed length of description to 255.

--- trunk/sql/private_expenses.sql
+++ trunk/sql/private_expenses.sql
@@ -3,6 +3,6 @@ CREATE TABLE `private_expenses` (
   `amount` decimal(10,2) NOT NULL default '0.00',
   `is_incoming` tinyint(1) NOT NULL default '0',  
   `datum` date NOT NULL default '2006-07-01',
-  `description` varchar(100) NOT NULL default '',
+  `description` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
