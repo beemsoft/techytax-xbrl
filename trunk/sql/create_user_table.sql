b3a396ae07977efe8ab8e568f231d66f5ac993fe
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2012-01-25 16:03:56 +0000

    Added company name in fiscal overview title.

--- trunk/sql/create_user_table.sql
+++ trunk/sql/create_user_table.sql
@@ -4,10 +4,12 @@ CREATE TABLE `user` (
   `role` VARCHAR(20) NOT NULL,  
   `password` varchar(50) NOT NULL,
   `fullName` varchar(100) NOT NULL,
+  `companyName` varchar(100),  
   `latestOnlineTime` datetime NULL default '2006-07-01 00:00:00',
   `blocked` tinyint(1) default '0',  
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO `user` VALUES (0,'guest','guest','guest','Guest',null);
+
 
