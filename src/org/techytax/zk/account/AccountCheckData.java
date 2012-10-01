603239863b2be967526415f7940d2170e8b543e8
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2012-10-01 20:42:03 +0000

    Migrating to ZK.

---
+++ src/org/techytax/zk/account/AccountCheckData.java
@@ -0,0 +1,88 @@
+package org.techytax.zk.account;
+
+import java.math.BigDecimal;
+
+public class AccountCheckData {
+
+	private BigDecimal accountBalance;
+	private BigDecimal grossIncome;
+	private BigDecimal paidInvoices;
+	private BigDecimal taxBalance;
+	private BigDecimal costBalance;
+	private BigDecimal savingBalance;
+	private BigDecimal privateWithdrawalBalance;
+	private BigDecimal interest;
+	private BigDecimal doubleCheck;
+
+	public BigDecimal getGrossIncome() {
+		return grossIncome;
+	}
+
+	public BigDecimal getPaidInvoices() {
+		return paidInvoices;
+	}
+
+	public BigDecimal getTaxBalance() {
+		return taxBalance;
+	}
+
+	public BigDecimal getCostBalance() {
+		return costBalance;
+	}
+
+	public BigDecimal getSavingBalance() {
+		return savingBalance;
+	}
+
+	public BigDecimal getPrivateWithdrawalBalance() {
+		return privateWithdrawalBalance;
+	}
+
+	public BigDecimal getInterest() {
+		return interest;
+	}
+
+	public void setGrossIncome(BigDecimal grossIncome) {
+		this.grossIncome = grossIncome;
+	}
+
+	public void setPaidInvoices(BigDecimal paidInvoices) {
+		this.paidInvoices = paidInvoices;
+	}
+
+	public void setTaxBalance(BigDecimal taxBalance) {
+		this.taxBalance = taxBalance;
+	}
+
+	public void setCostBalance(BigDecimal costBalance) {
+		this.costBalance = costBalance;
+	}
+
+	public void setSavingBalance(BigDecimal savingBalance) {
+		this.savingBalance = savingBalance;
+	}
+
+	public void setPrivateWithdrawalBalance(BigDecimal privateWithdrawalBalance) {
+		this.privateWithdrawalBalance = privateWithdrawalBalance;
+	}
+
+	public void setInterest(BigDecimal interest) {
+		this.interest = interest;
+	}
+
+	public BigDecimal getDoubleCheck() {
+		return doubleCheck;
+	}
+
+	public void setDoubleCheck(BigDecimal doubleCheck) {
+		this.doubleCheck = doubleCheck;
+	}
+
+	public BigDecimal getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(BigDecimal accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+}
