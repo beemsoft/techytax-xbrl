e4d64a7d1344e42432fb3eed873a556e7464fee2
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2013-09-15 17:23:53 +0000

    Fixed unavailable exception for RSS reader.

--- trunk/src/org/techytax/zk/rss/RssReaderBelastingdienst.java
+++ trunk/src/org/techytax/zk/rss/RssReaderBelastingdienst.java
@@ -7,7 +7,7 @@ public class RssReaderBelastingdienst extends RssReader {
 	private static final long serialVersionUID = 4683057507461954305L;
 
 	@Init
-	public void init() throws Exception {
+	public void init() {
 		feedUrl = "http://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/themaoverstijgend/nieuws/rss/nieuwsfeed_actueel_zakelijk.xml";
 		super.init();
 	}
