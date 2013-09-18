463f188c22c41182b79b6ee7e69927b52dbaf80c
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2013-09-18 04:39:57 +0000

    Migrated income tax overview from Struts to ZK.

--- src/org/techytax/zk/fiscal/TaxVM.java
+++ src/org/techytax/zk/fiscal/TaxVM.java
@@ -42,6 +42,7 @@ import org.techytax.zk.login.UserCredentialManager;
 import org.zkoss.zk.ui.Session;
 import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Messagebox;
 
 public class TaxVM {
 
@@ -58,86 +59,102 @@ public class TaxVM {
 			fiscalYear = DateHelper.getYear(previousFiscalPeriod.getBeginDatum());
 			List<Cost> costs = boekDao.getKostLijst(DateHelper.getDate(previousFiscalPeriod.getBeginDatum()),
 					DateHelper.getDate(previousFiscalPeriod.getEindDatum()), "alles", Long.toString(user.getId()));
-			overview = FiscalOverviewHelper.createFiscalOverview(DateHelper.getDate(previousFiscalPeriod.getBeginDatum()),
-					DateHelper.getDate(previousFiscalPeriod.getEindDatum()), costs, user.getId().longValue(), locale);
-			AuditLogger.log(AuditType.TAX_OVERVIEW, user);
+			try {
+				overview = FiscalOverviewHelper.createFiscalOverview(DateHelper.getDate(previousFiscalPeriod.getBeginDatum()),
+						DateHelper.getDate(previousFiscalPeriod.getEindDatum()), costs, user.getId().longValue(), locale);
+				AuditLogger.log(AuditType.TAX_OVERVIEW, user);
+			} catch (Exception e) {
+				Messagebox.show(e.getMessage(), null, 0, Messagebox.ERROR);
+			}
 		}
 	}
-	
+
 	public int getFiscalYear() {
 		return fiscalYear;
 	}
 
 	public ListModelList<ReportTableItem> getProfitAndLossTableItems() {
 		List<ReportTableItem> items = new ArrayList<ReportTableItem>();
-		items.add(new ReportTableItem("Netto omzet", "", "", "", format(overview.getNettoOmzet())));
-		items.add(new ReportTableItem("Renteopbrengsten van banktegoeden", "", "", "", "+" + format(overview.getInterestFromBusinessSavings())));
-		items.add(new ReportTableItem("Inkooppkosten", "", "", "", "-" + format(overview.getRepurchase())));
-		items.add(new ReportTableItem("Afschrijving auto", "", "", "-" + format(overview.getAfschrijvingAuto()), ""));
-		items.add(new ReportTableItem("Afschrijvingen apparaten", "", "", "-" + format(overview.getAfschrijvingOverig()), ""));
-		items.add(new ReportTableItem("Afschrijving bedrijfsruimte", "", "", "-" + format(overview.getSettlementDepreciation()), ""));
-		items.add(new ReportTableItem("Totale afschrijving", "", "", "", "-" + format(overview.getAfschrijvingTotaal())));
-		items.add(new ReportTableItem("Bijtelling privégebruik", "+" + format(overview.getBijtellingAuto()), "", "", ""));
-		items.add(new ReportTableItem("Afschrijving auto", "-" + format(overview.getAfschrijvingAuto()), "", "", ""));
-		items.add(new ReportTableItem("Autokosten", "-" + format(overview.getKostenAuto()), "", "", ""));
-		items.add(new ReportTableItem("Aftrekbare autokosten", "", format(overview.getKostenAutoAftrekbaar()), "", ""));
-		items.add(new ReportTableItem("Overige transportkosten", "", "-" + format(overview.getKostenOverigTransport()), "", ""));
-		items.add(new ReportTableItem("Auto- en transportkosten", "", "", "-" + format(overview.getCarAndTransportCosts()), ""));
-		items.add(new ReportTableItem("Overige kosten", "", "", "-" + format(overview.getKostenOverig()), ""));
-		items.add(new ReportTableItem("Huisvestingskosten", "", "", "-" + format(overview.getSettlementCosts()), ""));
-		items.add(new ReportTableItem("Totaal overige bedrijfskosten", "", "", "", "-" + format(overview.getOtherCostsTotal())));
-		items.add(new ReportTableItem("Winst", "", "", "", format(overview.getProfit())));
-		items.add(new ReportTableItem("Wijzigingen in toelaatbare reserves, toevoeging oudedagsreserve", "", "", "", "("
-				+ format(overview.getOudedagsReserveMaximaal()) + ")"));
-		items.add(new ReportTableItem("Kleinschaligheidsinvesteringsaftrek", "", "", "", "(" + format(overview.getInvestmentDeduction()) + ")"));
+		if (overview != null) {
+			items.add(new ReportTableItem("Netto omzet", "", "", "", format(overview.getNettoOmzet())));
+			items.add(new ReportTableItem("Renteopbrengsten van banktegoeden", "", "", "", "+" + format(overview.getInterestFromBusinessSavings())));
+			items.add(new ReportTableItem("Inkooppkosten", "", "", "", "-" + format(overview.getRepurchase())));
+			items.add(new ReportTableItem("Afschrijving auto", "", "", "-" + format(overview.getAfschrijvingAuto()), ""));
+			items.add(new ReportTableItem("Afschrijvingen apparaten", "", "", "-" + format(overview.getAfschrijvingOverig()), ""));
+			items.add(new ReportTableItem("Afschrijving bedrijfsruimte", "", "", "-" + format(overview.getSettlementDepreciation()), ""));
+			items.add(new ReportTableItem("Totale afschrijving", "", "", "", "-" + format(overview.getAfschrijvingTotaal())));
+			items.add(new ReportTableItem("Bijtelling privégebruik", "+" + format(overview.getBijtellingAuto()), "", "", ""));
+			items.add(new ReportTableItem("Afschrijving auto", "-" + format(overview.getAfschrijvingAuto()), "", "", ""));
+			items.add(new ReportTableItem("Autokosten", "-" + format(overview.getKostenAuto()), "", "", ""));
+			items.add(new ReportTableItem("Aftrekbare autokosten", "", format(overview.getKostenAutoAftrekbaar()), "", ""));
+			items.add(new ReportTableItem("Overige transportkosten", "", "-" + format(overview.getKostenOverigTransport()), "", ""));
+			items.add(new ReportTableItem("Auto- en transportkosten", "", "", "-" + format(overview.getCarAndTransportCosts()), ""));
+			items.add(new ReportTableItem("Overige kosten", "", "", "-" + format(overview.getKostenOverig()), ""));
+			items.add(new ReportTableItem("Huisvestingskosten", "", "", "-" + format(overview.getSettlementCosts()), ""));
+			items.add(new ReportTableItem("Totaal overige bedrijfskosten", "", "", "", "-" + format(overview.getOtherCostsTotal())));
+			items.add(new ReportTableItem("Winst", "", "", "", format(overview.getProfit())));
+			items.add(new ReportTableItem("Wijzigingen in toelaatbare reserves, toevoeging oudedagsreserve", "", "", "", "("
+					+ format(overview.getOudedagsReserveMaximaal()) + ")"));
+			items.add(new ReportTableItem("Kleinschaligheidsinvesteringsaftrek", "", "", "", "(" + format(overview.getInvestmentDeduction()) + ")"));
+		}
 		return new ListModelList<ReportTableItem>(items);
 	}
 
 	public ListModelList<ReportTableItem> getActivaTableItems() {
-		BalanceReport report = overview.getActivaReport();
-		List<ReportBalance> balanceList = report.getBalanceList();
-
 		List<ReportTableItem> items = new ArrayList<ReportTableItem>();
-		for (ReportBalance balance : balanceList) {
-			items.add(new ReportTableItem(balance.getOmschrijving(), AmountHelper.formatDecimal(balance.getAanschafKosten()), format(balance
-					.getBookValueBegin()), format(balance.getBookValueEnd()), format(balance.getRestwaarde())));
+		if (overview != null) {
+			BalanceReport report = overview.getActivaReport();
+			List<ReportBalance> balanceList = report.getBalanceList();
+			for (ReportBalance balance : balanceList) {
+				items.add(new ReportTableItem(balance.getOmschrijving(), AmountHelper.formatDecimal(balance.getAanschafKosten()), format(balance
+						.getBookValueBegin()), format(balance.getBookValueEnd()), format(balance.getRestwaarde())));
+			}
+			items.add(new ReportTableItem("Totaal", "", format(report.getTotalBeginValue()), format(report.getTotalEndValue()), ""));
 		}
-		items.add(new ReportTableItem("Totaal", "", format(report.getTotalBeginValue()), format(report.getTotalEndValue()), ""));
 		return new ListModelList<ReportTableItem>(items);
 	}
 
 	public ListModelList<ReportTableItem> getPassivaTableItems() {
-		BalanceReport report = overview.getPassivaReport();
-		List<ReportBalance> balanceList = report.getBalanceList();
-
 		List<ReportTableItem> items = new ArrayList<ReportTableItem>();
-		for (ReportBalance balance : balanceList) {
-			items.add(new ReportTableItem(balance.getOmschrijving(), "", format(balance.getBookValueBegin()), format(balance.getBookValueEnd()), ""));
+		if (overview != null) {
+			BalanceReport report = overview.getPassivaReport();
+			List<ReportBalance> balanceList = report.getBalanceList();
+			for (ReportBalance balance : balanceList) {
+				items.add(new ReportTableItem(balance.getOmschrijving(), "", format(balance.getBookValueBegin()), format(balance.getBookValueEnd()),
+						""));
+			}
+			items.add(new ReportTableItem("Totaal", "", format(report.getTotalBeginValue()), format(report.getTotalEndValue()), ""));
+			items.add(new ReportTableItem("Ondernemingsvermogen", "", "", format(overview.getEnterpriseCapital()), ""));
 		}
-		items.add(new ReportTableItem("Totaal", "", format(report.getTotalBeginValue()), format(report.getTotalEndValue()), ""));
-		items.add(new ReportTableItem("Ondernemingsvermogen", "", "", format(overview.getEnterpriseCapital()), ""));
 		return new ListModelList<ReportTableItem>(items);
 	}
 
 	public ListModelList<ReportTableItem> getPrivatWithDrawalTableItems() {
 		List<ReportTableItem> items = new ArrayList<ReportTableItem>();
-		items.add(new ReportTableItem("Onttrekking in contanten", format(overview.getOnttrekking().getWithdrawalCash())));
-		items.add(new ReportTableItem("Onttrekking voor privégebruik zakelijke auto", format(overview.getOnttrekking()
-				.getWithdrawalPrivateUsageBusinessCar())));
-		items.add(new ReportTableItem("Totale onttrekking", format(overview.getOnttrekking().getTotaleOnttrekking())));
+		if (overview != null) {
+			items.add(new ReportTableItem("Onttrekking in contanten", format(overview.getOnttrekking().getWithdrawalCash())));
+			items.add(new ReportTableItem("Onttrekking voor privégebruik zakelijke auto", format(overview.getOnttrekking()
+					.getWithdrawalPrivateUsageBusinessCar())));
+			items.add(new ReportTableItem("Totale onttrekking", format(overview.getOnttrekking().getTotaleOnttrekking())));
+		}
 		return new ListModelList<ReportTableItem>(items);
 	}
 
 	public ListModelList<ReportTableItem> getPrivatDepositTableItems() {
 		List<ReportTableItem> items = new ArrayList<ReportTableItem>();
-		items.add(new ReportTableItem("Privéstortingen", format(overview.getPrivateDeposit())));
+		if (overview != null) {
+			items.add(new ReportTableItem("Privéstortingen", format(overview.getPrivateDeposit())));
+		}
 		return new ListModelList<ReportTableItem>(items);
 	}
 
 	public ListModelList<ReportTableItem> getPrepaidTaxTableItems() {
 		List<ReportTableItem> items = new ArrayList<ReportTableItem>();
-		items.add(new ReportTableItem("Voorlopige aanslag inkomstenbelasting en premie volksverzekeringen", format(overview.getPrepaidTax().getPrepaidIncome())));
-		items.add(new ReportTableItem("Inkomensafhankelijke bijdrage Zorgverzekeringswet", format(overview.getPrepaidTax().getPrepaidHealth())));
+		if (overview != null) {
+			items.add(new ReportTableItem("Voorlopige aanslag inkomstenbelasting en premie volksverzekeringen", format(overview.getPrepaidTax()
+					.getPrepaidIncome())));
+			items.add(new ReportTableItem("Inkomensafhankelijke bijdrage Zorgverzekeringswet", format(overview.getPrepaidTax().getPrepaidHealth())));
+		}
 		return new ListModelList<ReportTableItem>(items);
 	}
 
