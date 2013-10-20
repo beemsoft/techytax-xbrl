7335a080e02628e76d75fc277760306bf8b65b3e
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2013-10-20 08:44:53 +0000

    Migrated settlement page from Struts to ZK.

---
+++ trunk/src/org/techytax/zk/fiscal/SettlementVM.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2013 Hans Beemsterboer
+ * 
+ * This file is part of the TechyTax program.
+ *
+ * TechyTax is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TechyTax is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TechyTax; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.techytax.zk.fiscal;
+
+import java.util.Date;
+
+import org.techytax.dao.SettlementDao;
+import org.techytax.domain.Settlement;
+import org.techytax.domain.User;
+import org.techytax.util.DateHelper;
+import org.techytax.zk.login.UserCredentialManager;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+
+public class SettlementVM {
+
+	private User user = UserCredentialManager.getUser();
+	private SettlementDao settlementDao = new SettlementDao();
+	private Settlement settlement = new Settlement();
+	private Date startDate;
+
+	@Init
+	public void init() throws Exception {
+		if (user != null) {
+			settlement = settlementDao.getSettlement(user.getId());
+			startDate = DateHelper.stringToDate(settlement.getStartDate());
+		}
+	}
+	
+	@Command
+	public void save() throws Exception {
+		settlement.setUserId(user.getId());
+		settlement.setStartDate(DateHelper.getDate(startDate));
+		settlementDao.updateSettlement(settlement);
+	}	
+	
+	public String getAddress() {
+		return user.getCompanyAddress();
+	}
+
+	public Settlement getSettlement() {
+		return settlement;
+	}
+
+	public void setSettlement(Settlement settlement) {
+		this.settlement = settlement;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+}
