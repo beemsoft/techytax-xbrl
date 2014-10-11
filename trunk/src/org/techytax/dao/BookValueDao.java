dedb57350050805bde87be2d12a5f48bf0735d6c
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2014-10-11 15:28:47 +0000

    Bookyear closed

--- trunk/src/org/techytax/dao/BookValueDao.java
+++ trunk/src/org/techytax/dao/BookValueDao.java
@@ -65,5 +65,17 @@ public class BookValueDao extends BaseDao<BookValue> {
 	private List<BookValue> getBookValuesForBalanceTypes(int year, List<BalanceType> balanceTypes) {
 		return findByNamedQuery(BookValue.FOR_YEAR_AND_TYPES, with("year", year).and("balanceTypes", balanceTypes).parameters());
 	}
+	
+	@Override
+	public void persistEntity(BookValue entity) {
+		throw new IllegalStateException("Bookyear closed!");
+//		super.persistEntity(entity);
+	}
+	
+	@Override
+	public void merge(BookValue entity) {
+		throw new IllegalStateException("Bookyear closed!");
+//		super.merge(entity);
+	}
 
 }
