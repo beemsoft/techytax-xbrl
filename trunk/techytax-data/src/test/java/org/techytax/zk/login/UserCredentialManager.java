ef8f3ef858b8b6a53a94283e01d737f9232df360
beemsoft <beemsoft@bf9486de-a7c8-49c5-bf1b-d7c6240abbcc>
2014-10-28 22:02:12 +0000

    Mavenized.

---
+++ trunk/techytax-data/src/test/java/org/techytax/zk/login/UserCredentialManager.java
@@ -0,0 +1,13 @@
+//package org.techytax.zk.login;
+//
+//import org.techytax.domain.User;
+//import org.techytax.domain.UserEntity;
+//
+//public class UserCredentialManager {
+//
+//	public static User getUser() {
+//		UserEntity user = new UserEntity();
+//		user.setId(1L);
+//		return user;
+//	}
+//}
