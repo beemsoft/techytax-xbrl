ad5efe807ab99c8f1197edbf84a253d7d44a1505
beemsoft <hans@beemsoft.nl>
2015-07-29 09:15:03 +0200

    Use UTF-8 encoding for the euro sign.

--- techytax-domain/src/main/java/org/techytax/helper/AmountHelper.java
+++ techytax-domain/src/main/java/org/techytax/helper/AmountHelper.java
@@ -109,7 +109,7 @@ public class AmountHelper {
 		DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.GERMAN);
 		otherSymbols.setDecimalSeparator(',');
 		otherSymbols.setGroupingSeparator('.');
-		DecimalFormat df = new DecimalFormat("€ ###,###,###,##0", otherSymbols);
+		DecimalFormat df = new DecimalFormat("â‚¬ ###,###,###,##0", otherSymbols);
 		return df.format(amount.doubleValue());
 	}
 	
@@ -117,7 +117,7 @@ public class AmountHelper {
 		DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.GERMAN);
 		otherSymbols.setDecimalSeparator(',');
 		otherSymbols.setGroupingSeparator('.');
-		DecimalFormat df = new DecimalFormat("€ ###,###,###,##0.00", otherSymbols);
+		DecimalFormat df = new DecimalFormat("â‚¬ ###,###,###,##0.00", otherSymbols);
 		return df.format(amount.doubleValue());
 	}	
 	
